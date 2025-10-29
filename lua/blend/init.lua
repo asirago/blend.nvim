1dca6c3ebcff35cfbbc0c9c3c441c31caa410ae2
asirago <40822894+asirago@users.noreply.github.com>
2025-10-29 05:54:29 +0100

    feat/ add GoAddStructTag user command for adding struct tags

---
+++ lua/blend/init.lua
@@ -0,0 +1,23 @@
+local go = require("blend.go")
+local utils = require("blend.utils")
+local M = {}
+
+function M.setup(setup_opts)
+	utils.create_user_cmd("GoAddStructTag", function(opts)
+		if not utils.is_go_file() then
+			vim.notify("not a go file", vim.log.levels.ERROR)
+			return
+		end
+
+		local struct = go.get_struct_at_cursor()
+
+		if not struct then
+			vim.notify("struct not found", vim.log.levels.DEBUG)
+			return
+		end
+
+		go.add_tags(unpack(opts.fargs))
+	end, { nargs = "*", desc = "Add Go struct tags" }, "<leader>tag", setup_opts)
+end
+
+return M
