f9fe422ea113bd5f229dd7d67dada7f077be6db0
asirago <40822894+asirago@users.noreply.github.com>
2025-10-29 04:54:07 +0100

    utils/ add is_go_file helper and rename disable_default_keymaps

--- lua/blend/utils.lua
+++ lua/blend/utils.lua
@@ -2,14 +2,23 @@ local M = {}
 
 local map = vim.keymap.set
 
--- map("n", "<leader>tag", "<cmd>GoAddStructTag<cr>", { desc = "Adds struct tags to go structs [blend]" })
-
 function M.create_user_cmd(name, rhs, cmd_opts, keymap, opts)
 	vim.api.nvim_create_user_command(name, rhs, cmd_opts)
 
-	if not opts.disable_default_keymaps then
+	if not opts.disable_default then
 		map("n", keymap, "<cmd>" .. name .. "<cr>", { desc = cmd_opts.desc })
 	end
+end
+
+function M.is_go_file()
+	local ft = vim.api.nvim_get_option_value("ft", { buf = 0 })
+
+	if ft ~= "go" then
+		vim.notify("not a go file", vim.log.levels.ERROR, { title = "GoAddStructTag" })
+		return false
+	end
+
+	return true
 end
 
 return M
